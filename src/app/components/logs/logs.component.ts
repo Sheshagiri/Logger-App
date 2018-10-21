c9d1f16ea813c5797f102f76a7f34922d903294b
Sheshagiri <msheshagirirao@gmail.com>
2018-10-21 18:05:25 +0530

    added data from component.ts

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
-
+  logs: {
+    id: string,
+    text: string;
+    date: any
+  }[];
   constructor() { }
 
   ngOnInit() {
+    this.logs = [{
+      id: '1',
+      text: 'Generated components from ts',
+      date: new Date('12/27/2017')
+    },
+    {
+      id: '2',
+      text: 'Generated logs from ts',
+      date: new Date('10/02/2015')
+    },
+    {
+      id: '3',
+      text: 'Generated dummy data from ts',
+      date: new Date()
+    }
+    ];
   }
 
 }
